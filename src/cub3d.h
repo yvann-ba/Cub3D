f0493e7cb84c941e9fc42b3d6b23320db004287f
yvann-ba <barbot.yvann@gmail.com>
2024-09-03 10:44:22 +0200

    chore: Update .gitignore to ignore object files, executable, and new files

--- src/cub3d.h
+++ src/cub3d.h
@@ -91,4 +91,32 @@ void    clean_return(t_data *data);
 void	cleanup_graphics(t_ray *ray);
 
 
+typedef struct	s_ray
+{
+	double		posx; //position x du joueur
+	double		posy; //position y du joueur
+	double		dirx; //vecteur de direction (commence à -1 pour N, 1 pour S, 0 sinon)
+	double		diry; //vecteur de direction (commence à -1 pour W, 1 pour E, 0 sinon)
+	double		planx; //vecteur du plan (commence à 0.66 pour E, -0.66 pour W, 0 sinon)
+	double		plany; //vecteur du plan (commence à 0.66 pour N, -0.66 pour S, 0 sinon)
+	double		raydirx; //calcul de direction x du rayon
+	double		raydiry; //calcul de direction y du rayon
+	double		camerax; //point x sur la plan camera : Gauche ecran = -1, milieu = 0, droite = 1
+	int		mapx; // coordonée x du carré dans lequel est pos
+	int		mapy; // coordonnée y du carré dans lequel est pos
+	double		sidedistx; //distance que le rayon parcours jusqu'au premier point d'intersection vertical (=un coté x)
+	double		sidedisty; //distance que le rayon parcours jusqu'au premier point d'intersection horizontal (= un coté y)
+	double		deltadistx; //distance que rayon parcours entre chaque point d'intersection vertical
+	double		deltadisty; //distance que le rayon parcours entre chaque point d'intersection horizontal
+	int		stepx; // -1 si doit sauter un carre dans direction x negative, 1 dans la direction x positive
+	int		stepy; // -1 si doit sauter un carre dans la direction y negative, 1 dans la direction y positive
+	int		hit; // 1 si un mur a ete touche, 0 sinon
+	int		side; // 0 si c'est un cote x qui est touche (vertical), 1 si un cote y (horizontal)
+	double		perpwalldist; // distance du joueur au mur
+	int		lineheight; //hauteur de la ligne a dessiner
+	int		drawstart; //position de debut ou il faut dessiner
+	int		drawend; //position de fin ou il faut dessiner
+	int		x; //permet de parcourir tous les rayons
+}	
+
 #endif
--- src/main.c
+++ src/main.c
@@ -13,21 +13,15 @@ int main(int ac, char **av)
         exit(1);
     }
     init_ray(ray);
-
-    // if (ac != 2)
-    // {
-    //     ft_printf(RED"Error:\nNumber of arguments is incorrect:" WHITE"require format (./cub3d file.cub)");
-    //     return (1);
-    // }
-    // data = malloc(sizeof(t_data));
-    // if (!data)
-    // {
-    //     ft_printf("Error\nmalloc() failed\n", 2);
-    //     exit(1);
-    // }
-    // data->fd_map = open_file(av[1], data);
-    // data->read_file = read_file_to_string(data->fd_map, data);
-    // string_to_tab(data->read_file);
-    // clean_return(data);
-    return (0);
+    data->mlx = mlx_init();
+    data->mlx_win = mlx_new_window(data->mlx, WIN_WIDTH, WIN_HEIGHT, "Cub3D");
+    if (!data->mlx_win)
+    {
+        ft_printf("Error\nmlx_new_window() failed\n", 2);
+        exit(1);
+    }
+    data->img = mlx_new_image(data->mlx, WIN_WIDTH, WIN_HEIGHT);
+    data.addr = (int *)mlx_get_data_addr(data->img, &data->bpp, &data->line_length, &data->addr);
+    data.addr[]
+    mlx_loop(mlx);
 }
